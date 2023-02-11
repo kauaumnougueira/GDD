b802de694dae07b31fc15980826085f843a0dd58
kauaumonougueira <kaua_kna@hotmail.com>
2023-02-11 20:59:57 +0044

    funções de role criadas, sem funcionalidade

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +45,59 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function atribuirRoles(){
+        $role = Role::create(['name' => 'admin']);
+        $role = Role::create(['name' => 'lider']);
+        $role = Role::create(['name' => 'vice-lider']);
+        $role = Role::create(['name' => 'tesoureiro']);
+        $role = Role::create(['name' => 'secretario']);
+        $role = Role::create(['name' => 'membro']);
+
+
+
+        //permissões admin
+        $permission = Permission::create(['name' => 'all']); //rotas e permissões futuras somente para admins
+        $role = Role::findByName('admin');
+        $role->givePermissionTo($permission);
+
+        //permissões líder
+        $permission = Permission::create(['name' => 'criacao']);
+        $role = Role::findByName('lider');
+        $role->givePermissionTo($permission);
+
+        $permission = Permission::create(['name' => 'edicao']);
+        $role = Role::findByName('lider');
+        $role->givePermissionTo($permission);
+
+        //permissões tesoureiro
+        $permission = Permission::create(['name' => 'criacao']);
+        $role = Role::findByName('tesoureiro');
+        $role->givePermissionTo($permission);
+
+        //permissões secretário
+        $permission = Permission::create(['name' => 'criacao']);
+        $role = Role::findByName('secretario');
+        $role->givePermissionTo($permission);
+
+        //permissões membro (permissões desnecessárias, ja feitas pela auth)
+        $permission = Permission::create(['name' => 'visualização']);
+        $role = Role::findByName('membro');
+        $role->givePermissionTo($permission);
+
+
+        //essa parte de atribuição será definida pelo admin quando os membros estiverem cadastrados e aparecendo
+        $user = User::find(1);
+        $user->assignRole('admin'); //eu
+
+
+
+        if ($user->hasRole('lider')) {
+            //
+        }
+
+        if ($user->hasPermissionTo('edit articles')) {
+            //
+        }
+    }
 }
